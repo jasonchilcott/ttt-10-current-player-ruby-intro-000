05dbdd0e006aca69a137fb1b6ad28b3916b651ea
Jason Chilcott <jasonchilcott@me.com>
2020-05-18 21:48:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
   turns = 0
   board.each do |space|
-    if space == ("X" || "O")
+    if space == ("X")
+      turns += 1
+    elsif space == ("O")
       turns += 1
     end
   end
